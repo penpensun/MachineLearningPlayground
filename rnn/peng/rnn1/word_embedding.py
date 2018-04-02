81814b504503927f3814b4acb9ffd77d0b67a5aa
penpensun <penpen926@gmail.com>
2018-04-02 23:12:45 +0200

    rnn

--- rnn/peng/rnn1/word_embedding.py
+++ rnn/peng/rnn1/word_embedding.py
@@ -22,13 +22,15 @@ def maybe_download(filename, expected_bytes):
     '''
     Download a file if not present, and make sure it's the right size.
     '''
-    if 'win' in sys.platform:
+    if 'win32' in sys.platform:
         fullfilename = windows_dir+filename;
-    if 'mac' in sys.platform:
+    if 'darwin' in sys.platform:
         fullfilename = mac_dir +filename;
 
-    if 'win' in sys.platform:
+    if 'win32' in sys.platform:
         '''Use proxy'''
+        print('System platform %s'%sys.platform);
+        print("using proxy")
         proxy = urllib.request.ProxyHandler({'http':'http://MZMBI:FMRs5Tab@10.185.190.100:8080'});
         opener = urllib.request.build_opener(proxy);
         urllib.request.install_opener(opener);
@@ -45,131 +47,195 @@ def maybe_download(filename, expected_bytes):
         raise Exception('Fail to verify'+ filename+'. Can you get it with a browser');
     return filename;
 
+
+
 def read_data(filename):
-    '''Extract the first file enclosed in a zip file as a list of words'''
+    ''' Extract the zip file'''
     with zipfile.ZipFile(filename) as f:
         data = tf.compat.as_str(f.read(f.namelist()[0])).split();
     return data;
 
-vocabulary_size = 50000
 
-def build_dataset(words):
-    count = [['UNK', -1]];
-    count.extend(collections.Counter(words).most_common(vocabulary_size-1));
+def build_dataset(words, vocabulary_size):
+    count = [['UNK',-1]];
+    count.extend(collections.Counter(words).most_common(vocabulary_size -1));
     dictionary = dict();
     for word, _ in count:
         dictionary[word] = len(dictionary);
     data = list();
-    unk_count = 0;
+    unk_count =0;
     for word in words:
         if word in dictionary:
             index = dictionary[word];
         else:
-            index =0; #dictionary['UNK']
+            index =0;
             unk_count = unk_count+1;
         data.append(index);
     count[0][1] = unk_count;
-    reverse_dictionary = dict(zip(dictionary.values(), dictionary.keys()));
-    return data, count, dictionary, reverse_dictionary;
+    reverse_dictionary = dict(zip (dictionary.values(), dictionary.keys()));
+    return data,count, dictionary, reverse_dictionary;
 
-
-
-def generate_batch(batch_size,num_skips, skip_window, data):
+def generate_batch(batch_size, num_skips, skip_window, data ):
     global data_index;
     assert batch_size % num_skips == 0;
-    assert num_skips <= 2* skip_window;
+    assert num_skips <= 2 * skip_window;
     batch = np.ndarray(shape = (batch_size),dtype = np.int32);
-    labels = np.ndarray(shape = (batch_size,1),dtype = np.int32);
-
-    span = 2*skip_window+1;# [skip_window target skip_window]
+    labels = np.ndarray(shape = (batch_size, 1), dtype = np.int32);
+    span = 2*skip_window+1;
     buffer = collections.deque(maxlen = span);
-
     for _ in range(span):
         buffer.append(data[data_index]);
-        data_index = (data_index+1)% len(data);
-
-    for i in range(batch_size // num_skips):
-        target = skip_window;
-        targets_to_avoid = [skip_window];
-        for j in range(num_skips):
+        data_index = (data_index +1);
+    for i in range (batch_size // num_skips):
+        target = skip_window; #target label at the center of the buffer
+        targets_to_avoid  = [skip_window];
+        for j in range (num_skips):
             while target in targets_to_avoid:
-                target = random.randint(0,span-1);
+                target = random.randint(0, span -1);
             targets_to_avoid.append(target);
-            batch[i*num_skips+j] = buffer[skip_window];
+            batch[i*num_skips+j ]  = buffer[skip_window];
             labels[i*num_skips+j,0] = buffer[target];
         buffer.append(data[data_index]);
-        data_index = (data_index+1)%len(data);
+        data_index = (data_index+1) %len(data);
     return batch, labels;
 
 
-filename = maybe_download('text8.zip', 31344016);
-data_index = 0;
-words = read_data(filename);
-print('Data size %d' %len(words));
-data,count, dictionary, reverse_dictionary = build_dataset(words);
-print('Most common words (+UNK) ',count[:5]);
-print('Sample data', data[:10]);
-
-print('data:', [reverse_dictionary[di] for di in data[:8] ]);
-for num_skips, skip_window in [(2,1), (4,2)]:
-    data_index = 0;
-    batch, labels = generate_batch(batch_size = 8, num_skips = num_skips, skip_window = skip_window, data = data);
-    print('\n with num_skips = %d and skip_window = %d: '% (num_skips, skip_window));
-    print('  batch:', [reverse_dictionary[bi] for bi in batch]);
-    print('  labels:', [reverse_dictionary[li] for li in labels.reshape(8)]);
-
-#Print out the data
-#print("Print out the data:");
-#print(data);
-
-batch_size = 128;
-embedding_size = 128; #dimension of the embedding vector.
-skip_window = 1; #how many words to consider left and right
-num_skips = 2; # how many times to reuse an input to generate a label
-
-valid_size = 16 #Random set of words to evaluate similarity on.
-valid_window = 100 #Only pick dev samples in the head of the distribution
-valid_examples = np.array(random.sample(range(valid_window), valid_size));
-num_sampled = 64; # Number of negative examples to sample
-
-
 def training():
-    graph = tf.Graph();
-    with graph.as_default():
+    global batch_size;
+    global embedding_size; #Dimension of the embedding vector
+    global skip_window; #How many words to consider left and right;
+    global num_skips; #How many times to reuse an input to generate a label
+    #
+    global valid_size;
+    global valid_window;
+    global valid_examples;
+    global num_sampled;
+    global vocabulary_size;
 
-        #Input data
+    graph = tf.Graph();
+    with graph.as_default(), tf.device('/cpu:0'):
+        # Input data.
         train_dataset = tf.placeholder(tf.int32, shape = [batch_size]);
-        train_labels = tf.placeholder(tf.int32, shape= [batch_size,1]);
+        train_labels = tf.placeholder(tf.int32, shape = [batch_size, 1]);
         valid_dataset = tf.constant(valid_examples, dtype = tf.int32);
 
         #Variables
-        embeddings = tf.Variable(tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0));
-        softmax_weights = tf.Variable(tf.truncated_normal(
-            [vocabulary_size, embedding_size],stddev = 1.0/math.sqrt(embedding_size)));
+        embeddings = tf.Variable(
+            tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0)
+        );
+
+        softmax_weights = tf.Variable(
+            tf.truncated_normal([vocabulary_size, embedding_size],
+                                stddev = 1.0/math.sqrt(embedding_size))
+        );
 
         softmax_biases = tf.Variable(tf.zeros([vocabulary_size]));
 
-        #Model.
-        #Look up embeddings for inputs.
+        #Model
+        #Look up embeddings for inputs
         embed = tf.nn.embedding_lookup(embeddings, train_dataset);
         # Compute the softmax loss, using a sample of the negative labels each time.
         loss = tf.reduce_mean(
-            tf.nn.sampled_softmax_loss(softmax_weights, softmax_biases, embed, train_labels, num_sampled, vocabulary_size)
+            tf.nn.sampled_softmax_loss(weights = softmax_weights, biases = softmax_biases,
+                                       inputs = embed, labels = train_labels, num_sampled = num_sampled,
+                                       num_classes = vocabulary_size)
         );
-
         #Optimizer
-        # The embeddings are also optimized, because the embeddings are also as variables defined.
+        # The optimizer will optimize the softmax_weights and the embeddings
         optimizer = tf.train.AdagradOptimizer(1.0).minimize(loss);
 
-    #Start training
-    num_steps = 10001;
+        #Compute the similarity between minibatch example and all embededings.
+        # We use cosine distance:
+        norm = tf.sqrt(tf.reduce_sum(tf.square(embeddings), 1, keep_dims = True));
+        normalized_embeddings = embeddings/norm;
+        valid_embeddings = tf.nn.embedding_lookup(normalized_embeddings, valid_dataset);
+        similarity = tf.matmul(valid_embeddings, tf.transpose(normalized_embeddings));
+
+    global num_steps;
+    global data;
+    global dictionary, reverse_dictionary;
+
     with tf.Session(graph = graph) as session:
-        tf.initialize_all_variables();
-        print("Initialized.");
+        session.run(tf.global_variables_initializer());
+        print("Initialized");
         average_loss = 0;
         for step in range(num_steps):
-            batch_data, batch_labels = generate_batch(batch_size, num_skips, skip_window);
+            batch_data, batch_labels = generate_batch(batch_size, num_skips, skip_window, data);
             feed_dict = {train_dataset: batch_data, train_labels: batch_labels};
-            _, l = session.run([optimizer, loss], feed_dict = feed_dict);
+            _,l = session.run([optimizer, loss], feed_dict = feed_dict);
+            average_loss += l;
+
+            if step % 2000 == 0:
+                if step >0:
+                    average_loss = average_loss / 2000;
+                print("Average loss at step %d: %f " % (step, average_loss));
+                average_loss = 0;
+            '''
+            # Note that this is expensive
+            if step % 5000 == 0:
+                sim = similarity.eval();
+                for i in range(valid_size):
+                    valid_word = reverse_dictionary[valid_examples[i]];
+                    top_k = 8;
+                    nearest = (-sim[i,:]).argsort()[1:top_k+1];
+                    log = 'Nearest to %s' %valid_word
+                    for k in range(top_k):
+                        close_word = reverse_dictionary[nearest[k]];
+                        log = '%s %s'%(log,close_word);
+                    print(log);
+            '''
+        final_embedding = normalized_embeddings.eval(); #Compute the final word embeddings.
+    return final_embedding;
+
+
+def plot(embeddings, labels):
+    assert embeddings.shape[0] >= len(labels), 'More labels than embeddings'
+    pylab.figure(figsize = (15,15));
+
+    for i ,label in enumerate(labels):
+        x,y = embeddings[i,:];
+        pylab.scatter(x,y);
+        pylab.annotate(label, xy= (x,y), xytext = (5,2), textcoords = 'offset points', ha = 'right', va = 'bottom')
+    pylab.show();
+
+if __name__ == '__main__':
+    filename = maybe_download('text8.zip',31344016);
+    words = read_data(filename);
+    print("Data size %d"%(len(words)));
+    vocabulary_size = 50000;
+    data,count,dictionary, reverse_dictionary = build_dataset(words, vocabulary_size);
+    print("Most common words (+UNK)", count[:5]);
+    print('Sample data', data[:10]);
+
+    data_index = 0;
+    print("data: ",[reverse_dictionary[di] for di in data[:8]]);
+    for num_skips, skip_window in [(2,1),(4,2)]:
+        data_index = 0;
+        batch,labels = generate_batch(batch_size =8, num_skips = num_skips, skip_window = skip_window, data = data);
+        print("\n with num_skips = %d and skip_window = %d" %(num_skips, skip_window));
+        print("  batch: ", [reverse_dictionary[bi] for bi in batch]);
+        print("  labels: ", [reverse_dictionary[li] for li in labels.reshape(8)]);
+
+    batch_size = 128;
+    embedding_size = 128; #Dimension of the embedding vector.
+    skip_window = 1; #How many words to consider left and right
+    num_skips = 2; #How many times to reuse an input to generate a label.
+    valid_size = 16; #Random set of words to evaluate similarity on.
+    valid_window = 100 #Only pick dev samples in the head of the distribution.
+    valid_examples = np.array(random.sample(range(valid_window), valid_size));
+    num_sampled = 64; #Number of negative examples to sample.
+    num_steps = 100001;
+
+    final_embeddings = training();
+
+    num_points = 400;
+    tsne = TSNE(perplexity = 30, n_components = 2, init = 'pca', n_iter = 5000, method = 'exact');
+    two_d_embeddings = tsne.fit_transform(final_embeddings[1:num_points+1, :]);
+
+    words = [reverse_dictionary[i] for i in range(1, num_points+1)];
+    plot(two_d_embeddings, words);
+
+
+
 
 
